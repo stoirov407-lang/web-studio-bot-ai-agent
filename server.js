be371f1ae78f33d0343d22ec13ebdc0a544f5a50
stoirov407-lang <stoirov407@gmail.com>
2026-08-24 02:53:24 +0500

    Implement CORS middleware and trust proxy
    Added CORS headers and trust proxy setting.

--- server.js
+++ server.js
@@ -28,8 +28,14 @@ const {
   PORT = 3000,
 } = process.env;
 
-const app = express();
-app.set("trust proxy",1)
+app.set("trust proxy", 1);
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  if (req.method === "OPTIONS") return res.sendStatus(200);
+  next();
+});
 app.use(express.json({ limit: "2mb" }));
 
 // ---------- Роли агентов ----------
